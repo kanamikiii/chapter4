33db2ef0e7cd621097ce0fee53a2e450bbf15bfc
tung <doan.viet.tung@framgia.com>
2014-06-23 10:33:59 +0700

    follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   #get "users/new"
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   resources :sessions, only: [:new, :create, :destroy]
 
   resources :microposts, only: [:create, :destroy]
+
+  resources :relationships, only: [:create, :destroy]
 
   #get 'static_pages/home'
   root  'static_pages#home'
